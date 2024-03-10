dfaf6748c945ad0147c069dc756f4daa9083a2a8
vinhwin1810 <vinhwin1810@gmail.com>
2024-03-09 19:53:08 -0500

    done

---
+++ 2540. Minimum Common Value.py
@@ -0,0 +1,15 @@
+# Two pointer Easy
+
+
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i, j = 0, 0
+
+        while i < len(nums1) and j  < len(nums2):
+            if nums1[i] < nums2[j]:
+                i+=1
+            elif nums1[i] == nums2[j]:
+                return nums1[i]
+            else:
+                j+=1
+        return -1
