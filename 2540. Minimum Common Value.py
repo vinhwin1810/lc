59e9f1fa889918ad6dc434bdbe9983920c51b709
vinhwin1810 <vinhwin1810@gmail.com>
2024-03-09 20:40:04 -0500

    O(n)

--- 2540. Minimum Common Value.py
+++ 2540. Minimum Common Value.py
@@ -1,15 +1,13 @@
-# Two pointer Easy
-
-
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        i, j = 0, 0
+        # Add the elements from nums1 to set1
+        set1 = set(nums1)
 
-        while i < len(nums1) and j  < len(nums2):
-            if nums1[i] < nums2[j]:
-                i+=1
-            elif nums1[i] == nums2[j]:
-                return nums1[i]
-            else:
-                j+=1
+        # Search for each element of nums2 in set1
+        # Return the first common element found
+        for num in nums2:
+            if num in set1:
+                return num
+
+        # Return -1 if there are no common elements
         return -1
