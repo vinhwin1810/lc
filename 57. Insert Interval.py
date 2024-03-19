c204d029c89b19902f7eb923573ecb1b1c9a6ea7
vinhwin1810 <vinhwin1810@gmail.com>
2024-03-18 21:24:24 -0400

    fix

--- 57. Insert Interval.py
+++ 57. Insert Interval.py
@@ -31,15 +31,3 @@ class Solution:
     def merge(self, a, b):
         return [min(a[0], b[0]), max(a[1], b[1])]
         res = []
-        
-        # for i in range(len(intervals)):
-        #     if newInterval[1] < intervals[i][0]: #ending of new interval is less than starting of one of the intervals (not overlapping)
-        #         res.append(newInterval)
-        #         return res + intervals[i:]
-        #     elif newInterval[0] > intervals[i][1]: #start of new interval is greater than end of one of the intervals (not overlapping)
-        #         res.append(intervals[i])
-        #     else: #when intervals are overlapping, we will merge the intervals
-        #         newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-                
-        # res.append(newInterval)  #incase first if doesn't execute
-        # return res
