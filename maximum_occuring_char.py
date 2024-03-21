9a328550732898ac92c7b611cfd6a0cb2f2bc240
vinhwin1810 <vinhwin1810@gmail.com>
2024-03-21 19:51:53 -0400

    solving

--- maximum_occuring_char.py
+++ maximum_occuring_char.py
@@ -1,2 +1,13 @@
 def maximumOccuringCharacter(text):
-    
+    counter = {}
+
+    for char in text:
+        if char not in counter:
+            counter[char] = 1
+        else:
+            counter[char] +=1
+
+    maxVal = max(counter.values())
+
+    for char, val in counter.items():
+        if val == maxVal:
