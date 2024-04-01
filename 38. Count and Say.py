ab4d03c9530f615956b3784799670aeff0e15752
vinhwin1810 <vinhwin1810@gmail.com>
2024-04-01 19:41:32 -0400

    new and solve

---
+++ 38. Count and Say.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        
+        previous_res = self.countAndSay(n-1)
+
+        res = ""
+
+        i = 1
+        sub = previous_res[0]
+        while i < len(previous_res):
+
+            if previous_res[i] == sub[-1]:
+                sub+=previous_res[i]
+            else:
+                res += str(len(sub))
+                res+= sub[-1]
+                sub = previous_res[i]
+            i+=1
+        
+        res += str(len(sub))
+        res+= sub[-1]
+
+        return res
