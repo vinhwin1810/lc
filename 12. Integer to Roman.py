85ef1237e63d53f17d3933daaaa531df52d4f7f3
vinhwin1810 <vinhwin1810@gmail.com>
2024-04-04 06:33:16 -0400

    solve

--- 12. Integer to Roman.py
+++ 12. Integer to Roman.py
@@ -1,1 +1,17 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        num_to_roman = {
+                    1000: "M", 900: "CM", 500: "D", 400: "CD",
+                    100: "C", 90: "XC", 50: "L", 40: "XL",
+                    10: "X", 9: "IX", 5: "V", 4: "IV",
+                    1: "I"
+                }
+        res = ''
+        for val, char in num_to_roman.items():
+            while num >= val:
+                res += char
+                num -= val
+                
+        return res
 
+    
