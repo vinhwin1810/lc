1d2f591bb716c60c89e3e6c50dff4c3d94703230
vinhwin1810 <vinhwin1810@gmail.com>
2024-04-05 02:03:48 -0400

    new and solve

---
+++ 1544. Make The String Great.py
@@ -0,0 +1,12 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+
+        for char in s:
+            if stack and (char.islower() and char.upper() == stack[-1] or 
+                          char.isupper() and char.lower() == stack[-1]):
+                stack.pop()
+            else:
+                stack.append(char)
+
+        return ''.join(stack)
