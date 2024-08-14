0978bdd357b6ee17ac430b200ad9d86687fe6a0f
vinhwin1810 <vinhwin1810@gmail.com>
2024-08-14 18:42:33 -0400

    new

--- Max Area of Island.py
+++ Max Area of Island.py
@@ -5,28 +5,26 @@ class Solution:
 
         max_area = 0
 
-        rows, cols = len(grid), len(grid[0])
+        rows , cols = len(grid), len(grid[0])
         visited = set()
 
         def dfs(r, c):
-            if r not in range(rows) or c not in range(cols) or (r, c) in visited or grid[r][c] == 0:
+            if r not in range(rows) or c not in range(cols) or (r,c) in visited or grid[r][c] == 0:
                 return 0
             
-            visited.add((r, c))
-            
+            visited.add((r,c))
             area = 1
-            
-            area += dfs(r, c + 1)
-            area += dfs(r + 1, c)
-            area += dfs(r - 1, c)
-            area += dfs(r, c - 1)
-            
+            area += dfs(r, c+1)
+            area += dfs(r+1, c)
+            area += dfs(r-1, c)
+            area += dfs(r, c-1)
+
             return area
 
         for r in range(rows):
             for c in range(cols):
-                if (r, c) not in visited and grid[r][c] == 1:
-                    cur_area = dfs(r, c)
+                if (r,c) not in visited and grid[r][c] == 1:
+                    cur_area = dfs(r,c)
                     max_area = max(max_area, cur_area)
-        
+        print(max_area)
         return max_area
