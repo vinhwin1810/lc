9b34fac6363e89a7c3a1957f5142bf26f5fd56ac
vinhwin1810 <vinhwin1810@gmail.com>
2024-08-29 14:14:26 -0400

    Quick

--- codepath.py
+++ codepath.py
@@ -1,9 +1,32 @@
-def addFun(n:int):
-    if n <=0 :
-        return 0
-    if n == 1:
-        return 2
-    return addFun(n-1) + addFun(n-2)
+# def addFun(n:int):
+#     if n <=0 :
+#         return 0
+#     if n == 1:
+#         return 2
+#     return addFun(n-1) + addFun(n-2)
 
-print(addFun(6))
+# print(addFun(6))
     
+
+# The functino is expectyed to return an INTEGER_SIGNLY_LINKEDLIST.
+# The function accepts INTEGER_SINGLY_LINKED_LIST head as parameter.
+
+class SinglyLinkedListNode:
+    int data
+    SinglyLinkedListNode next
+
+def deleteEven(listHead: SinglyLinkedListNode) -> SinglyLinkedListNode:
+    if not listHead:
+        return None
+    
+    dummy = SinglyLinkedListNode(0)
+    dummy.next = listHead
+    current = dummy
+    
+    while current.next:
+        if current.next.data % 2 == 0:
+            current.next = current.next.next
+        else:
+            current = current.next
+    
+    return dummy.next
