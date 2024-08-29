a97c49f3d9536f252d25c2482277960e432e8b3e
vinhwin1810 <vinhwin1810@gmail.com>
2024-08-29 15:32:58 -0400

    Quick

--- codepath.py
+++ codepath.py
@@ -131,3 +131,53 @@ if __name__ == '__main__':
 
     fptr.close()
 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'getUmbrellas' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER requirement
+#  2. INTEGER_ARRAY sizes
+#
+
+def getUmbrellas(requirement, sizes):
+    # Write your code here
+    dp = [requirement + 1] * (requirement + 1)
+    dp[0] = 0
+
+    for a in range(1, requirement + 1):
+        for c in sizes:
+            if a - c >= 0:
+                dp[a] = min(dp[a], 1 + dp[a - c])
+    return dp[requirement] if dp[requirement] != requirement + 1 else -1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    requirement = int(input().strip())
+
+    sizes_count = int(input().strip())
+
+    sizes = []
+
+    for _ in range(sizes_count):
+        sizes_item = int(input().strip())
+        sizes.append(sizes_item)
+
+    result = getUmbrellas(requirement, sizes)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
