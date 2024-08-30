717b4f8b967cbab13a7fb7a7ebdd2068e0b1641e
vinhwin1810 <vinhwin1810@gmail.com>
2024-08-30 10:38:03 -0400

    Quick

--- binary_tree_diameter.py
+++ binary_tree_diameter.py
@@ -7,8 +7,20 @@
 
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        
+        res = 0
+
+        def dfs(root):
+            nonlocal res
+
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            res = max(res, left + right)
+
+            return 1 + max(left, right)
+
+        dfs(root)
+        return res
         
         
