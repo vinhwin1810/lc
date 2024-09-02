306ef28975da38b3408ba97bc961e5f187f256bf
vinhwin1810 <vinhwin1810@gmail.com>
2024-09-02 11:17:44 -0400

    Automated commit

--- decode.py
+++ decode.py
@@ -20,7 +20,7 @@
             line_number += 1
 # Modified by script
 
-    # Concatenate the message words to form the decoded message
+# Modified by script
 # Modified by script
 # Modified by script
 # Modified by script
