41edc88888df051199300add7f551fafd9314fd0
vinhwin1810 <vinhwin1810@gmail.com>
2024-09-05 15:58:42 -0400

    Automated commit

--- coinbase.py
+++ coinbase.py
@@ -7,7 +7,7 @@ def solution(numbers):
 # Modified by script
         
 # Modified by script
-            if (window[0] < window[1] and window[1] > window[2]) or (window[0] > window[1] and window[1] < window[2]):
+# Modified by script
                 res.append(1)
 # Modified by script
                 res.append(0)
