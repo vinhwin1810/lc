cfaa5275ac6dbfe97a2dd1aeb4b2c8a829b976b1
vinhwin1810 <vinhwin1810@gmail.com>
2024-09-06 00:36:51 -0400

    Automated commit

--- coinbase.py
+++ coinbase.py
@@ -10,7 +10,7 @@
 # Modified by script
                 res.append(1)
 # Modified by script
-                res.append(0)
+# Modified by script
 # Modified by script
 # Modified by script
 # Modified by script
